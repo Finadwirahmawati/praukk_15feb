0733dead5484302c1f46b4dc1f9bbde5491c66e4
Finadwirahmawati <109191106+Finadwirahmawati@users.noreply.github.com>
2023-02-15 14:50:14 +0700

    Add files via upload

---
+++ hapus.php
@@ -0,0 +1,17 @@
+<?php
+include("koneksi.php");
+$id = $_GET['id_dvd'];
+
+$sql= "DELETE FROM tb_penyewa where id_penyewa='$id'";
+$query = mysqli_query($db, $sql);
+
+$sql= "DELETE FROM tb_dvd where id_dvd='$id'";
+$query = mysqli_query($db, $sql);
+
+if($query){
+    header('Location:tampil.php?status=sukses');
+}else{
+
+    die("akses dilarang");
+}
+?>
